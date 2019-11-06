8aefa132a89616848f36a046707a66cbdb49747e
maxwen <max.weninger@gmail.com>
2019-11-06 19:34:00 +0530

    crDroidSettings: Suspend Actions [3/3]

--- src/com/crdroid/settings/fragments/Miscellaneous.java
+++ src/com/crdroid/settings/fragments/Miscellaneous.java
@@ -52,6 +52,7 @@ import com.crdroid.settings.fragments.misc.GamingMode;
 import com.crdroid.settings.fragments.misc.ImeSettings;
 import com.crdroid.settings.fragments.misc.SmartCharging;
 import com.crdroid.settings.fragments.misc.SensorBlock;
+import com.crdroid.settings.preferences.SystemSettingMasterSwitchPreference;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -64,9 +65,11 @@ public class Miscellaneous extends SettingsPreferenceFragment
 
     private static final String SHOW_CPU_INFO_KEY = "show_cpu_info";
     private static final String SMART_CHARGING = "smart_charging";
+    private static final String SCREEN_STATE_TOGGLES_ENABLE = "screen_state_toggles_enable_key";
 
     private SwitchPreference mShowCpuInfo;
     private Preference mSmartCharging;
+    private SystemSettingMasterSwitchPreference mEnableScreenStateToggles;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -90,6 +93,12 @@ public class Miscellaneous extends SettingsPreferenceFragment
                 com.android.internal.R.bool.config_smartChargingAvailable);
         if (!mSmartChargingSupported)
             prefScreen.removePreference(mSmartCharging);
+
+        mEnableScreenStateToggles = (SystemSettingMasterSwitchPreference) findPreference(SCREEN_STATE_TOGGLES_ENABLE);
+        int enabled = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.START_SCREEN_STATE_SERVICE, 0, UserHandle.USER_CURRENT);
+        mEnableScreenStateToggles.setChecked(enabled != 0);
+        mEnableScreenStateToggles.setOnPreferenceChangeListener(this);
     }
 
     public static void reset(Context mContext) {
@@ -136,6 +145,19 @@ public class Miscellaneous extends SettingsPreferenceFragment
         Context mContext = getActivity().getApplicationContext();
         if (preference == mShowCpuInfo) {
             writeCpuInfoOptions(mContext, (Boolean) newValue);
+            return true;
+        } else if (preference == mEnableScreenStateToggles) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.START_SCREEN_STATE_SERVICE, value ? 1 : 0, UserHandle.USER_CURRENT);
+            Intent service = (new Intent())
+                .setClassName("com.android.systemui", "com.android.systemui.crdroid.screenstate.ScreenStateService");
+            if (value) {
+                getActivity().stopService(service);
+                getActivity().startService(service);
+            } else {
+                getActivity().stopService(service);
+            }
             return true;
         }
         return false;
